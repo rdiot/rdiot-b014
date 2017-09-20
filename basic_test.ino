c11b8aec309d57d6a03d01b868c75834003ca65c
rdiot <rd1004g@gmail.com>
2017-09-20 23:31:56 +0900

    Create basic_test.ino

---
+++ basic_test.ino
@@ -0,0 +1,14 @@
+/* ITEAD 3G Shield (IM121026002) [B014] : http://rdiot.tistory.com/32 [RDIoT Demo] */
+
+void setup()
+{
+  Serial.begin(115200);
+}
+void loop()
+{ 
+  while(1)
+  { 
+    Serial.print(ATD01012341234;r);
+    delay(5000);
+  }
+}
